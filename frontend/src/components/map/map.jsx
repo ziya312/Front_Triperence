7c7a0cbdf37f019cfc782b7f730e85cd7abf6b5c
ziya312 <92806642+ziya312@users.noreply.github.com>
2022-08-03 14:04:40 +0900

    Update map.jsx

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { React, useState } from 'react';
 import { GoogleMap, InfoWindow, LoadScript, Marker, MarkerClusterer } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '82rem',
-  height: '856px'
+  height: '856px',
 };
 
 const center = {
@@ -11,45 +11,60 @@ const center = {
   lng: 126.451,
 };
 
-
-const onLoad = marker => {
-  console.log('marker: ', marker)
-}
+const onLoad = (marker) => {
+  console.log('marker: ', marker);
+};
 
 const options = {
   imagePath:
     'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m', // so you must have m1.png, m2.png, m3.png, m4.png, m5.png and m6.png in that folder
-}
+};
 
-function Map({place}) {
+function Map({ place }) {
+  const [selectedCenter, setSelectedCenter] = useState(null);
   return (
     <LoadScript
       googleMapsApiKey="AIzaSyCrXhf7dS8MZ1tiCUiy-y-yVfy_GToWCNA"
-      language='en'
+      language="en"
       region="KR"
     >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      >
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
         <MarkerClusterer options={options}>
-        {(clusterer) =>
+          {(clusterer) =>
             place.map((place) => (
               <Marker
-              icon={{url:'https://drive.google.com/uc?id=1Mt5tR73ZyY-DCPZx_svJ3EBaeEQG8BBT'}}
-              key={place.contentid} 
-              position={{lat: place.mapy, lng: place.mapx}} 
-              clusterer={clusterer} />
+                icon={{ url: 'https://drive.google.com/uc?id=107OwTILQ7WeO8YRqxW-RHUAU9oBr3W2J' }}
+                key={place.contentid}
+                position={{ lat: place.mapy, lng: place.mapx }}
+                onClick={() => {
+                  setSelectedCenter(center);
+                }}
+                clusterer={clusterer}
+              />
             ))
           }
         </MarkerClusterer>
-        
-        { /* Child components, such as markers, info windows, etc. */ }
+        {selectedCenter && (
+          <InfoWindow
+            onCloseClick={() => {
+              setSelectedCenter(null);
+            }}
+            position={{
+              lat: selectedCenter.latitude,
+              lng: selectedCenter.longitude,
+            }}
+          >
+            <div>
+              <h3>{selectedCenter.title}</h3>
+            </div>
+          </InfoWindow>
+        )}
+
+        {/* Child components, such as markers, info windows, etc. */}
         <></>
       </GoogleMap>
     </LoadScript>
-  )
+  );
 }
 
-export default React.memo(Map)
+export default Map;
